dd161c640e6e79c71255be3ac3323216e425ff1a
Alex Howley <howley.alex@gmail.com>
2024-02-14 15:06:57 -0600

    add sender interface

---
+++ data/src/interface/sender.rs
@@ -0,0 +1,91 @@
+use super::{IdInterface, ShapeInterface};
+use sqlx::{query, Postgres, Transaction};
+
+pub struct Sender<'a> {
+    pub sender_name: &'a str,
+    pub campaign_id: i32,
+    pub is_censored: bool,
+}
+
+impl<'a, 'tr> ShapeInterface<'a, 'tr> for Sender<'a> {
+    type Shape = (String, i32, bool);
+
+    async fn from_values(values_tuple: &'a Self::Shape) -> Self {
+        Self {
+            sender_name: &values_tuple.0,
+            campaign_id: values_tuple.1,
+            is_censored: values_tuple.2,
+        }
+    }
+
+    async fn try_fetch_values(
+        transaction: &'a mut Transaction<'tr, Postgres>,
+        id: i32,
+    ) -> sqlx::Result<Self::Shape> {
+        let values = query!(
+            r#"SELECT sender_name, campaign_id, is_censored
+            FROM sender
+            WHERE id = $1"#,
+            id
+        )
+        .fetch_one(&mut **transaction)
+        .await?;
+
+        Ok((values.sender_name, values.campaign_id, values.is_censored))
+    }
+}
+
+impl<'a, 'tr> IdInterface<'a, 'tr> for Sender<'a> {
+    type IdType = i32;
+
+    async fn try_fetch_id(
+        &self,
+        transaction: &'a mut Transaction<'tr, Postgres>,
+    ) -> sqlx::Result<Self::IdType> {
+        let id = query!(
+            r#"SELECT id FROM sender
+            WHERE sender_name = $1 AND campaign_id = $2 AND is_censored = $3"#,
+            self.sender_name,
+            self.campaign_id,
+            self.is_censored,
+        )
+        .fetch_one(&mut **transaction)
+        .await?
+        .id;
+
+        Ok(id)
+    }
+
+    async fn try_insert(
+        &self,
+        transaction: &'a mut Transaction<'tr, Postgres>,
+    ) -> sqlx::Result<Self::IdType> {
+        let id = query!(
+            r#"INSERT INTO sender (sender_name, campaign_id, is_censored)
+            VALUES ( $1, $2, $3 )
+            RETURNING id
+            "#,
+            self.sender_name,
+            self.campaign_id,
+            self.is_censored,
+        )
+        .fetch_one(&mut **transaction)
+        .await?
+        .id;
+
+        Ok(id)
+    }
+
+    async fn fetch_or_insert_id(
+        &self,
+        transaction: &'a mut Transaction<'tr, Postgres>,
+    ) -> Self::IdType {
+        if let Ok(id) = self.try_fetch_id(&mut *transaction).await {
+            return id;
+        }
+
+        self.try_insert(&mut *transaction)
+            .await
+            .expect("failed to insert new sender record")
+    }
+}
